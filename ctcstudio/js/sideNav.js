724f4ffb0ae29123b2dbaddc9ce37e1c8714ff79
MAPLELEAF3659 <ig910425@gmail.com>
2021-11-14 22:25:42 +0800

    Delete ctcstudio directory

--- ctcstudio/js/sideNav.js
+++
@@ -1,9 +0,0 @@
-$(".sideNav").sidebar({ speed: 400 });
-
-$(".header-nav").click(function() {
-    $(".sideNav").trigger("sidebar:open");
-});
-
-$(".sideNav-btn-close").click(function() {
-    $(".sideNav").trigger("sidebar:close");
-});
